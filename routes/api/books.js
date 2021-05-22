fa8d750d8d1ed3750e8e10912ddc1a1a058aad1f
Gary B <grider27@gmail.com>
2021-05-22 10:38:45 -0400

    break out controllers instead of 1 route file

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,43 +1,17 @@
 const router = require("express").Router();
-var db = require("../../models");
+const booksController = require("../../controllers/booksController");
 
-// "/api/books" routes
-router.get("/api/books", (req, res) => {
-    db.Book
-        .find(req.query)
-        .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
 
-router.post("/api/books", (req, res) => {
-    db.Book
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
+// Matches with "/api/books"
+router.route("/")
+    .get(booksController.findAll)
+    .post(booksController.create);
 
-// "/api/books/:id" routes
-router.get("/api/books/:id", (req, res) => {
-    db.Book
-        .findById(req.params.id)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
-
-router.put("/api/books/:id", (req, res) => {
-    db.Book
-        .findOneAndUpdate({ _id: req.params.id }, req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
-
-router.delete("/api/books/:id", (req, res) => {
-    db.Book
-        .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-})
+// Matches with "/api/books/:id"
+router
+    .route("/:id")
+    .get(booksController.findById)
+    .put(booksController.update)
+    .delete(booksController.remove);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,13 @@
+const path = require("path");
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const path = require("path");
 
 // API Routes
 router.use("/api", apiRoutes);
 
 // Send every other request to the React app
-// router.use(function (req, res) {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-router.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
