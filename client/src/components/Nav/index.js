2c2dcf13665df1513ad51f1aae749881856d2dbf
Gary B <grider27@gmail.com>
2021-05-22 11:03:37 -0400

    main navbar

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./style.css";
+
+function MainNav() {
+  return (
+    <Navbar variant="dark" expand="lg">
+      <Navbar.Brand>
+        Google Books Search
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link className="nav-item nav-link h5" to="/">
+            Search
+          </Link>
+          <Link className="nav-item nav-link h5" to="/saved">
+            Saved
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default MainNav;
